558a2315151cc756f2593272c6ebfbdf1beaedb6
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-02-12 00:11:40 +0500

    initial commit

---
+++ 5-slots/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <select v-model="componentChange">
+    <option value="Register">Register</option>
+    <option value="Contact">Contact</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentChange"></component>
+  </keep-alive>
+</template>
+
+<script>
+import Register from "./components/Register.vue";
+import Contact from "./components/Contact.vue";
+
+export default {
+  name: "App",
+  components: {
+    Register,
+    Contact,
+  },
+  data() {
+    return {
+      componentChange: "Register",
+    };
+  },
+};
+</script>
---
+++ 5-slots/src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+  </form>
+</template>
