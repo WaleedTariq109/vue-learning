835e3c5de293d43e04c969040ac663ab667c47eb
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-10-22 05:15:59 +0500

    slots

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -1,47 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <app-form>
+    <template v-slot:help>
+      <p>This is some help text</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="Enter Name" />
+      <input type="email" placeholder="Enter Email" />
+      <input type="password" placeholder="Enter Password" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </app-form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <app-form>
+    <template v-slot:help>
+      <p>This is Contact Form</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" placeholder="Enter Name" />
+      <input type="email" placeholder="Enter Email" />
+    </template>
+    <template v-slot:buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </app-form>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script>
+import AppForm from '@/components/Form.vue';
+export default {
+  name: 'App',
+  components: {
+    AppForm,
+  },
+};
+</script>
