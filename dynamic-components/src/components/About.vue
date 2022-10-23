320a942c5a98357bd4e07dbf9d480e596a2c2b9f
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-10-23 05:25:51 +0500

    dynamic component

---
+++ dynamic-components/src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>This is About Page</h1>
+</template>
+
+<script>
+export default {
+  name: 'About',
+
+  activated() {
+    console.log('About is Activated');
+  },
+
+  deactivated() {
+    console.log('About is Deactivated');
+  },
+};
+</script>
---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>This is Home Page</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  mounted() {
+    console.log('Maybe not work on 2nd time because of KeepAlive');
+  },
+
+  activated() {
+    console.log('Home is Activated');
+  },
+
+  deactivated() {
+    console.log('Home is Deactivated');
+  },
+};
+</script>
