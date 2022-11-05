34900f5703a26854e979a479e72f1ae018d7780a
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-11-06 04:42:06 +0500

    Update App.vue

--- animations/src/App.vue
+++ animations/src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <button type="button" @click="flag = !flag">Toggle</button>
+  <button
+    type="button"
+    @click="flag = !flag"
+  >
+    Toggle
+  </button>
 
   <!-- <Transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello</h2>
@@ -10,7 +15,7 @@
     <h2 v-if="flag">Zoom</h2>
   </Transition> -->
 
-  <Transition
+  <!-- <Transition
     @before-enter="beforeEnter"
     @enter="enter"
     @after-enter="afterEnter"
@@ -20,7 +25,26 @@
     :css="false"
   >
     <h2 v-if="flag">Hello</h2>
-  </Transition>
+  </Transition> -->
+
+  <button
+    type="button"
+    @click="addNumber"
+  >
+    Add Number
+  </button>
+
+  <ul>
+    <TransitionGroup name="list">
+      <li
+        v-for="(number, index) in numbers"
+        :key="index"
+        @click="removeNumber(index)"
+      >
+        {{ number }}
+      </li>
+    </TransitionGroup>
+  </ul>
 </template>
 
 <script>
@@ -29,31 +53,47 @@ export default {
   data() {
     return {
       flag: true,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
-    beforeEnter(el) {},
-    enter(el, done) {
-      const animation = el.animate([{ transform: 'scale3d(0,0,0)' }, {}], {
-        duration: 1000,
-      });
-      animation.onfinish = () => done();
+    addNumber() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
     },
-    afterEnter(el) {},
 
-    beforeLeave(el) {},
-    leave(el, done) {
-      const animation = el.animate([{}, { transform: 'scale3d(0,0,0)' }], {
-        duration: 1000,
-      });
-      animation.onfinish = () => done();
+    removeNumber(index) {
+      this.numbers.splice(index, 1);
     },
-    afterLeave(el) {},
+
+    // beforeEnter(el) {},
+    // enter(el, done) {
+    //   const animation = el.animate([{ transform: 'scale3d(0,0,0)' }, {}], {
+    //     duration: 1000,
+    //   });
+    //   animation.onfinish = () => done();
+    // },
+    // afterEnter(el) {},
+
+    // beforeLeave(el) {},
+    // leave(el, done) {
+    //   const animation = el.animate([{}, { transform: 'scale3d(0,0,0)' }], {
+    //     duration: 1000,
+    //   });
+    //   animation.onfinish = () => done();
+    // },
+    // afterLeave(el) {},
   },
 };
 </script>
 
 <style>
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
 h2 {
   width: 400px;
   padding: 20px;
@@ -65,17 +105,44 @@ h2 {
   opacity: 0;
 }
 
-.fade-enter-to {
-  opacity: 1;
-}
-
 .fade-enter-active {
-  transition: all 0.2s ease-in-out;
+  transition: all 1s linear;
 }
 
 .fade-leave-to {
-  transition: all 0.2s ease-in-out;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+.fade-move {
+  transition: all 1s linear;
+}
+
+.list-enter-active,
+.list-leave-active,
+.list-move {
+  transition: 500ms cubic-bezier(0.59, 0.12, 0.34, 0.95);
+  transition-property: opacity, transform;
+}
+
+.list-enter {
+  opacity: 0;
+  transform: translateX(50px) scaleY(0.5);
+}
+
+.list-enter-to {
+  opacity: 1;
+  transform: translateX(0) scaleY(1);
+}
+
+.list-leave-active {
+  position: absolute;
+}
+
+.list-leave-to {
+  opacity: 0;
+  transform: scaleY(0);
+  transform-origin: center top;
 }
 
 .zoom-enter-active {
