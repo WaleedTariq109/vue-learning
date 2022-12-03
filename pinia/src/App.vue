c637729d550f33ccea2bdbb82a3e65609d880758
Waleed Tariq <76990392+hadAbyte@users.noreply.github.com>
2022-12-03 18:59:21 +0500

    update

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -2,7 +2,29 @@
   <main>
     <header>
       <img src="@/assets/pinia-logo.svg" alt="pinia-logo" />
-      <h1>Pinia Tasks</h1>
+      <h1>{{ taskStore.name }}</h1>
     </header>
+
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+import { useTaskStore } from "@/stores/TasksStore";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+
+    return {
+      taskStore,
+    };
+  },
+};
+</script>
--- pinia/src/stores/TasksStore.js
+++ pinia/src/stores/TasksStore.js
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 
 export const useTaskStore = defineStore("task-store", {
   state: () => ({
+    name: "Pinia Tasks",
     tasks: [
       { id: 1, title: "Buy Some Milk", isFav: false },
       { id: 2, title: "Play Guitar", isFav: true },
